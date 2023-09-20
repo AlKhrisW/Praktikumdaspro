beef2684be7a22e49c056edf087f3a7df22af3d6
unknown <aldokhrisna@gmail.com>
2023-09-20 11:52:51 +0700

    Halo

---
+++ ContohTipeData03.java
@@ -0,0 +1,16 @@
+public class ContohTipeData03 {
+    public static void main(String[] args) {
+        char golonganDarah = 'o';
+        short jumlahPenduduk = 1025;
+        float auhhu = 60.50f;
+        double berat = 0.5467812345;
+        long saldo = 150000000;
+        int angkaDesimal = 0x10;
+
+        System.out.println("Golongan darah\t: " + (byte)golonganDarah);
+        System.out.println("Jarak\t\t: " + jumlahPenduduk);
+        System.out.println("Jumlah Penduduk\t: " + jumlahPenduduk);
+        System.out.println("Suhu\t\t: " + suhu);
+        
+    }
+}
