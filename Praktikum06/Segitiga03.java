7096d04b9845e8c2f90a8d93712f285a29e2ebda
unknown <aldokhrisna@gmail.com>
2023-10-11 13:12:41 +0700

    Percobaan 2 no 1-2

--- Praktikum06/Segitiga03.java
+++ Praktikum06/Segitiga03.java
@@ -19,11 +19,17 @@ public class Segitiga03 {
         if(totalSudut == 180){
             if((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)){
             System.out.println("Segitiga tersebut segitiga siku-siku");
-            } else {
-                System.out.println("Segitiga tersebut bukan segitiga siku-siku");
+            } else if((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60)){
+                System.out.println("Segitiga tersebut segitiga sama sisi");
+            } else if((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3)){
+                System.out.println("Segitiga tersebut segitiga sama kaki");
+            } else{
+                System.out.println("Segitiga tersebut segitiga sembarang");
             }
         }else{
             System.out.println("bukan segitiga");
         }
+        
+        sc.close();
     }
 }
