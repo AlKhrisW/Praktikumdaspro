2ba3fa19a4d120916dd641fbbcfd6f1ade7d5b26
unknown <aldokhrisna@gmail.com>
2023-10-11 15:33:16 +0700

    Percobaan 4 no 3-4

--- Praktikum06/Bilangan03.java
+++ Praktikum06/Bilangan03.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Bilangan03 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
 
-       int bilangan1, bilangan2, bilangan3;
+       int bilangan1, bilangan2, bilangan3, bilanganTerbesar;
 
        System.out.print("Masukkan bilangan 1: ");
        bilangan1 = sc.nextInt();
@@ -13,28 +13,11 @@ public class Bilangan03 {
        System.out.print("Masukkan bilangan 3: ");
        bilangan3 = sc.nextInt();
 
-       if(bilangan1 > bilangan2){
-         if(bilangan2 > bilangan3){
-               System.out.println("Bilangan terbesar: " + bilangan1);
-         } else{
-            if(bilangan1 > bilangan3){
-               System.out.println("Bilangan terbesar: " + bilangan1);
-            } else{
-               System.out.println("Bilangan terbesar: " + bilangan3);
-            }
-         }
-      }else{
-         if(bilangan1 > bilangan3){
-            System.out.println("Bilangan terbesar: " + bilangan2);
-         }else{
-            if(bilangan2 > bilangan3){
-               System.out.println("Bilangan terbesar: " + bilangan2);
-            }else{
-               System.out.println("Bilangan terbesar: " + bilangan3);
-            }
-         }
-      }
+       bilanganTerbesar = bilangan1 > bilangan2 ? (bilangan1 > bilangan3 ? bilangan1 : (bilangan1 > bilangan3 ? bilangan1 : bilangan3)) : (bilangan1 > bilangan3 ? bilangan2 : (bilangan2 > bilangan3 ? bilangan2 : bilangan3));
+
+       System.out.print("bilangan terbesar: " + bilanganTerbesar);
+       bilanganTerbesar = sc.nextInt();
 
       sc.close();
-    }
+   }
 }
