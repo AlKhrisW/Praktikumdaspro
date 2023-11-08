8c47483f78e780b67e45e5fe01def7805376d3f2
AlKhrisW <aldokhrisna@gmail.com>
2023-11-08 13:40:22 +0700

    Percobaan 2

---
+++ Praktikum10/BioskopWithScanner03.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BioskopWithScanner03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int baris, kolom;
+        String nama, next;
+        String[][] penonton = new String[4][2];
+
+        while (true) {
+            System.out.println("Masukkan nama: ");
+            nama = sc.next();
+            System.out.println("Masukkan baris: ");
+            baris = sc.nextInt();
+            System.out.println("Masukkan kolom: ");
+            kolom = sc.nextInt();
+
+            penonton[baris-1][kolom-1] = nama;
+
+            System.out.println("Input penonton lainnya? (y/n): ");
+            next = sc.next();
+
+            if (next.equals("n")) {
+                break;
+            }
+        }
+    }
+}
