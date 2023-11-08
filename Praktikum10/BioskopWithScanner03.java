1f734c2967fbe74531820fa10caaf57a48ecba06
AlKhrisW <aldokhrisna@gmail.com>
2023-11-08 15:55:14 +0700

    Percobaan 2

--- Praktikum10/BioskopWithScanner03.java
+++ Praktikum10/BioskopWithScanner03.java
@@ -4,26 +4,57 @@ public class BioskopWithScanner03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int baris, kolom;
+        int baris, kolom, menu;
         String nama, next;
         String[][] penonton = new String[4][2];
 
-        while (true) {
-            System.out.println("Masukkan nama: ");
-            nama = sc.next();
-            System.out.println("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.println("Masukkan kolom: ");
-            kolom = sc.nextInt();
+        do {
+            System.out.println("===== MENU =====");
+            System.out.println("1. Input Data Penonton");
+            System.out.println("2. Tampilkan Daftar Penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih Menu: ");
+            menu = sc.nextInt();
 
-            penonton[baris-1][kolom-1] = nama;
+            if (menu == 1) {
+                for (int i = 0; i < penonton.length; i++) {
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.next();
+                    System.out.print("Masukkan baris: ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom: ");
+                    kolom = sc.nextInt();
+    
+                    if (penonton[baris-1][kolom-1] == null){
+                        penonton[baris-1][kolom-1] = nama;
+                    } else {
+                        System.out.println("Kursi Tidak Tersedia");
+                        continue;
+                    }
 
-            System.out.println("Input penonton lainnya? (y/n): ");
-            next = sc.next();
+                    System.out.println("Input penonton lain? (y/n)");
+                    next = sc.next();
 
-            if (next.equals("n")) {
-                break;
+                    if (next.equalsIgnoreCase("n")) {
+                        break;
+                    }
+                }
+            } else if (menu == 2) {
+                String simbol = "***";
+                for (int i = 0; i < penonton.length; i++) {
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        if (penonton[i][j] == null) {
+                            penonton[i][j] = simbol;
+                        }
+                    }
+                }
+                System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+                System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+                System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+                System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
             }
-        }
+        } while (menu != 3);
+        
+        sc.close();
     }
 }
