ee8a60fa6231e6b4de47794cdebf9ed639d9f721
AlKhrisW <aldokhrisna@gmail.com>
2023-11-11 06:28:18 +0700

    Praktikum 01

---
+++ Praktikum01/Latihan03.java
@@ -0,0 +1,7 @@
+public class Latihan03 {
+    public static void main(String[] args) {
+        System.out.print("\nNama saya Aldo");
+        //System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+
+    }
+}
---
+++ Praktikum01/Tugas03.java
@@ -0,0 +1,10 @@
+public class Tugas03 {
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama    : Aldo Khrisna Wijaya");
+        System.out.println("Absen   : 03");
+        System.out.println("Kelas   : 1D");
+        System.out.println("Prodi   : D4 Sistem Informasi Bisnis");
+        System.out.println("Jurusan : Teknologi Informasi");
+    }
+}
