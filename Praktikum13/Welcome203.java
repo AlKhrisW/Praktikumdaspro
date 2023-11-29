65693ed5792c4f2ae2861835b4d7480588ca2b85
AlKhrisW <aldokhrisna@gmail.com>
2023-11-29 14:29:02 +0700

    Percobaan 2

---
+++ Praktikum13/Welcome203.java
@@ -0,0 +1,12 @@
+public class Welcome203 {
+    public static void main(String[] args) {
+        displayWelcomeMassage("Ani");
+        displayWelcomeMassage("Budi");
+        displayWelcomeMassage("Cica");
+        displayWelcomeMassage("Dodi");
+
+    }
+    public static void displayWelcomeMassage(String name) {
+        System.out.println("Selamat Datang di Politeknik Negeri Malang, " + name);
+    }
+}
