04756d01a04a39cb17b987f4f4a847a236d5c424
AlKhrisW <aldokhrisna@gmail.com>
2023-11-29 14:50:39 +0700

    Percobaan 5

--- Praktikum13/LocalGlobal03.java
+++ Praktikum13/LocalGlobal03.java
@@ -3,6 +3,7 @@ public class LocalGlobal03 {
     public static void main(String[] args) {
         int b = 10;
         int c = 17;
+        a = a + 8;
 
         System.out.println("Nilai a: " + a);
         System.out.println("Nilai b: " + b);
@@ -13,6 +14,7 @@ public class LocalGlobal03 {
     private static void display() {
         int b = 20;
         int c = 5;
+        a = a + 12;
 
         System.out.println("Nilai a: " + a);
         System.out.println("Nilai b: " + b);
